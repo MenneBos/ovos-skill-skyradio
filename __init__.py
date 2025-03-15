6a26119cd22b8ed75751cb5af465100f56ee877c
Menne Bos <mennebos@gmail.com>
2025-03-16 00:16:42 +0100

    Add dialog responses for Playskyradio and Stopskyradio intents

--- __init__.py
+++ __init__.py
@@ -8,31 +8,21 @@ class SkyRadioSkill(OVOSSkill):
         super().__init__("SkyRadioSkill")
 
     def initialize(self):
-        self.add_event('mycroft.skyradio.play', self.handle_play_skyradio)
+        # self.add_event('mycroft.skyradio.play', self.handle_play_skyradio)
         self.register_intent_file('Playskyradio.intent', self.handle_play_skyradio)
+        self.register_intent_file('Stopskyradio.intent', self.handle_stop_skyradio)
 
-    def handle_Playskyradio():
+    def handle_play_skyradio():
         url = f"http://192.168.1.45/api/manager/logic/webhook/Terre/?tag=SkyRadio"
         data = requests.get(url)
         print(data.json())
-        self.speak_dialog("Please enjoy the music", wait=True)
+        self.speak_dialog("Playskyradio", wait=True)
 
-    def handle_Stopskyradio():
+    def handle_stop_skyradio():
         url = f"http://192.168.1.45/api/manager/logic/webhook/Terre/?tag=StopSkyRadio"
         data = requests.get(url)
         print(data.json())
-        self.speak_dialog("No worry, we can always start the radio again", wait=True)
-    
-
-    # Route intents to appropriate functions
-    def on_intent(intent_request):
-        intent_name = intent_request['intent']['name']
-        if intent_name == "Playskyradio":
-            return handle_Playskyradio()
-        elif intent_name == "Stopskyradio":
-            return handle_Stopskyradio()
-        else:
-            return "Sorry my love, I didn't to stop or start skyradio."
+        self.speak_dialog("Stopskyradio", wait=True)
 
 def create_skill():
     return SkyRadioSkill()
