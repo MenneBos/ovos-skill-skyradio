e2945fea6a562b8d689c23aad4850328900b03a4
Menne Bos <mennebos@gmail.com>
2025-03-15 22:14:18 +0100

    Add play and stop functionality for SkyRadioSkill

--- __init__.py
+++ __init__.py
@@ -11,10 +11,28 @@ class SkyRadioSkill(OVOSSkill):
         self.add_event('mycroft.skyradio.play', self.handle_play_skyradio)
         self.register_intent_file('Playskyradio.intent', self.handle_play_skyradio)
 
-    def handle_play_skyradio(self, message: Message):
+    def handle_Playskyradio():
         url = f"http://192.168.1.45/api/manager/logic/webhook/Terre/?tag=SkyRadio"
         data = requests.get(url)
         print(data.json())
+        self.speak_dialog("Please enjoy the music", wait=True)
+
+    def handle_Stopskyradio():
+        url = f"http://192.168.1.45/api/manager/logic/webhook/Terre/?tag=StopSkyRadio"
+        data = requests.get(url)
+        print(data.json())
+        self.speak_dialog("No worry, we can always start the radio again", wait=True)
+    
+
+# Route intents to appropriate functions
+    def on_intent(intent_request):
+        intent_name = intent_request['intent']['name']
+        if intent_name == "Playskyradio":
+            return handle_Playskyradio()
+        elif intent_name == "Stopskyradio":
+            return handle_Stopskyradio()
+        else:
+            return "Sorry my love, I didn't to stop or start skyradio."
 
 def create_skill():
     return SkyRadioSkill()
